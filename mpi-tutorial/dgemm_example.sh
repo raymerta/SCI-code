0b4b0de71142ff6d8131ec0b4c6423aeab5f7822
Liem Radita Tapaning Hesti <hesti@rocket.hpc.ut.ee>
2018-05-09 10:00:15 +0300

    modify dgemm example

--- mpi-tutorial/dgemm_example.sh
+++ mpi-tutorial/dgemm_example.sh
@@ -6,5 +6,8 @@
 module purge
 module load intel_parallel_studio_xe_2015
 
-export LANG=C
-icc -mkl dgemm_example.c
+export LANG=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+icc -mkl dgemm_example.c -o dgemm_example
